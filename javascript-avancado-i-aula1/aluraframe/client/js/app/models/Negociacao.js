d3733dbd2922b4a09bfc34642fa62c6fcba37acb
danielemaraschin <danikmaraschin@gmail.com>
2020-10-04 02:14:33 +0200

    working with encapsulation (get, this._data, ...)

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -1,9 +1,25 @@
 export class Negociacao{
     constructor(){
 
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
-        
+        this._data = new Date();
+        this._quantidade = 1;
+        this._valor = 0.0;  
+    }
+
+    getVolume(){
+
+        return this.quantidade * this.valor;
+    }
+
+    getData(){
+        return this._data;
+    }
+
+    getQuantidade(){
+        return this._quantidade;
+    }
+
+    getValor(){
+        return this._valor;
     }
 }
