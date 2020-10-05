ec13c16f525c1669a0b652f99c8825229198d2fd
danielemaraschin <danikmaraschin@gmail.com>
2020-10-05 03:15:43 +0200

    Date: .getTime, new Date - defensive programming

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -1,26 +1,26 @@
 export class Negociacao{
     constructor(){
 
-        this._data = data;
+        this._data = new Date (data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;  
         Object.freeze(this);
     }
 
-    get Volume(){
+    get volume(){
 
-        return this.quantidade * this.valor;
+        return this._quantidade * this._valor;
     }
 
-    get Data(){
-        return this._data;
+    get data(){
+        return new Date(this._data.getTime());
     }
 
-    get Quantidade(){
+    get quantidade(){
         return this._quantidade;
     }
 
-    get Valor(){
+    get valor(){
         return this._valor;
     }
 }
