d3659742b8b1ec781ba15a8d6b3c78dd1bfefbc5
danielemaraschin <danikmaraschin@gmail.com>
2020-10-07 08:09:04 +0200

    Fixing a small mistake ( ', ' instead of '-'

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -13,7 +13,7 @@ class NegociacaoController {
     adiciona (event){
         event.preventDefault();
 
-        let data = new Date (this._inputData.value.replace(/-/g, '-'));       
+        let data = new Date (this._inputData.value.replace(/-/g, ','));       
         console.log(data);
     }
 }
