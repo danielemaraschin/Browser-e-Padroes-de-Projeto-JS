0809298b78e976bf09a91b55c7a9a945ac5aefd7
danielemaraschin <danikmaraschin@gmail.com>
2020-10-09 06:11:05 +0200

    Arrow function, .map and ternary operator

--- javascript-avancado-i-aula1/aluraframe/client/exercicios/array-dobra-valoresPares-arrow-function copy.js
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/array-dobra-valoresPares-arrow-function copy.js
@@ -1,6 +1,8 @@
 let numeros = [3,2,11,20,8,7];
+
 let novosNumeros = 
-    numeros.map(item =>  item % 2 ?
-     item * 2
-     : item);
+ numeros.map(item => item %2 ?
+    item :
+    item *2)
+
 console.log(novosNumeros);
