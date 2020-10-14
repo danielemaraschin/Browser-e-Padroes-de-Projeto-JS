60bf35398207cba18067ee902b945be741100085
danielemaraschin <danikmaraschin@gmail.com>
2020-10-14 08:25:00 +0200

    working with static method and constructor

--- javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,12 +1,15 @@
 class DateHelper {
 
-    dataParaTexto(data) {
-        return data.getDate()
-        + '/' + (negociacao.data.getMonth() +1)
-        + '/' + negociacao.data.getFullYear();   // full year (4 numbers = 1990) pq se colocar so year sera so os 2 ultimos numeros (=90)
+    constructor(){
+        throw new Error ('Esta classe não pode ser instanciada');
     }
 
-    textoParaData(texto){
+
+    static dataParaTexto(data) {
+        return `${data.getDate()} / ${(negociacao.data.getMonth() +1} / ${negociacao.data.getFullYear()}`;
+    }
+
+    static textoParaData(texto){
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2)); 
     }
 
@@ -15,4 +18,4 @@ class DateHelper {
 
 /*...spread operator = desmembra o array e o primeiro item do array, e cada parâmetro do Date será posicionado na mesma ordem no construtor
 split: separador '-' da string, tranformando-a em um array
-.map(metodo q pode receber indice e/ou item do array como parametro da funcao)
+.map(metodo q pode receber indice e/ou item do array como parametro da funcao)*/
