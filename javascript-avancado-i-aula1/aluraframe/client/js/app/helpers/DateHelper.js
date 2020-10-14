21acd729b1525e21cd6d729f8b9270c8125ee00c
danielemaraschin <danikmaraschin@gmail.com>
2020-10-14 14:22:10 +0200

    Learning about  regex

--- javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
@@ -6,16 +6,26 @@ class DateHelper {
 
 
     static dataParaTexto(data) {
-        return `${data.getDate()} / ${(negociacao.data.getMonth() +1} / ${negociacao.data.getFullYear()}`;
+        return `${data.getDate()}/${(data.getMonth()+1}/${data.getFullYear()}`;
     }
 
     static textoParaData(texto){
+
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)){
+            throw new Error('Deve estar no formato aaaa-mm-dd')
+        }
+
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2)); 
     }
-
-
 }
 
-/*...spread operator = desmembra o array e o primeiro item do array, e cada parâmetro do Date será posicionado na mesma ordem no construtor
+
+
+
+
+
+
+/*spread operator = desmembra o array e o primeiro item do array, e cada parâmetro do Date será posicionado na mesma ordem no construtor
 split: separador '-' da string, tranformando-a em um array
 .map(metodo q pode receber indice e/ou item do array como parametro da funcao)*/
+hn
