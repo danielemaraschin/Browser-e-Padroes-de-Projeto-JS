f916d322e09d61c95f4e071b405c2784b806a985
danielemaraschin <danikmaraschin@gmail.com>
2020-10-19 09:02:57 +0200

    Working with static methods

---
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/guaraci.js
@@ -0,0 +1,10 @@
+class ConversorXML {
+
+    static converte(objeto) {
+        // converte um objeto em XML
+    }
+}
+
+let conversor = new ConversorXML();
+
+ConversorXML.converte({nome: 'Guaraci', idade: 40});
