4ae3b034f1067a1a55308ca03a8d03a776be3554
danielemaraschin <danikmaraschin@gmail.com>
2020-10-20 20:14:52 +0200

    Working with Views

--- javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
@@ -3,12 +3,11 @@ class DateHelper {
     constructor(){
         throw new Error ('Esta classe não pode ser instanciada');
     }
-
-
+    //Lançar erro no constructor para evitar que instanciem essa classe DateHelper
     static dataParaTexto(data) {
         return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`
     }
-    static textoParaData(texto){
+    static  textoParaData(texto){
 
         if(!/^\d{4}-\d{2}-\d{2}$/.test(texto))
             throw new Error('Deve estar no formato aaaa-mm-dd');
