5d29452576d1cd8f6508d5317857cdb946164fc2
danielemaraschin <danikmaraschin@gmail.com>
2020-10-23 20:16:29 +0200

    working with innerHTML, update, template string,..

--- javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,9 +1,9 @@
 class NegociacoesView {
-    constructor(){'
-        this._elemento = elemento;    
-}
-   
-    template(){
+    constructor(elemento) {
+        this._elemento = elemento;
+    }
+
+    _template() {
         return `
     
  <table class="table table-hover table-bordered">
@@ -15,7 +15,6 @@ class NegociacoesView {
                 <th>VOLUME</th>
             </tr>
         </thead>
-        
         <tbody>
         </tbody>
         <tfoot>
@@ -23,4 +22,14 @@ class NegociacoesView {
     </table>
         `;
     }
+
+    update(){
+    
+        // inerHTML transforma a string em elementos do DOM (se a string estiver com marcação correta) = será inserido como filho da div
+    
+        this._elemento.innerHTML = this._template(); // innerHTML é o retorno da função template; = retorno da template é filho da div;
+    }
+
+
+
 }
