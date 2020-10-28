d733edf80cc711b30b19974db74b0bd3009334cb
danielemaraschin <danikmaraschin@gmail.com>
2020-10-28 14:50:45 +0100

    Adding class 'mensagem' in constructor controller

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,9 +7,10 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
 
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem = new Mensagem ();
     }
 
     adiciona(event) {
