80e087620aa696c9a60df4e19e60a8a9c12ea426
danielemaraschin <danikmaraschin@gmail.com>
2020-10-31 10:07:42 +0100

    exercise about inheritance, displaying the values in index.html

---
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/herança/Heranca-de-conta/ContaController.js
@@ -0,0 +1,6 @@
+var conta1 = new ContaCorrente(200);
+var conta2 = new ContaPoupanca(300);
+conta1.atualiza(2);
+conta2.atualiza(3);
+console.log(conta1.saldo);
+console.log(conta2.saldo);
--- javascript-avancado-i-aula1/aluraframe/client/exercicios/herança/Heranca-de-conta/ContaPoupanca.js
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/herança/Heranca-de-conta/ContaPoupanca.js
@@ -1,6 +1,9 @@
 class ContaPoupanca extends Conta{
 
     atualiza(taxa){
+        this._saldo = this._saldo + (taxa*2);
+    }
+    atualiza2(taxa){
         return this._saldo + (taxa*2);
     }
 }
