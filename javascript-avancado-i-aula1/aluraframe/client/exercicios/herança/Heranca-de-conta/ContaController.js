831a06ac48f6d8ef837ddab58a0692ddeab4f4c5
danielemaraschin <danikmaraschin@gmail.com>
2020-10-31 11:05:01 +0100

    exercise with controller

--- javascript-avancado-i-aula1/aluraframe/client/exercicios/herança/Heranca-de-conta/ContaController.js
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/herança/Heranca-de-conta/ContaController.js
@@ -1,6 +1,19 @@
-var conta1 = new ContaCorrente(200);
-var conta2 = new ContaPoupanca(300);
-conta1.atualiza(2);
-conta2.atualiza(3);
-console.log(conta1.saldo);
-console.log(conta2.saldo);
+class ContaController{
+
+
+    mostraSaldo (){
+        var conta1 = new ContaCorrente(saldo);
+        var conta2 = new ContaPoupanca(saldo);
+        conta1.atualiza(2);
+        conta2.atualiza(3);
+        console.log(conta1.saldo);
+        console.log(conta2.saldo);
+    }
+}
+
+
+
+///////////////////////////
+
+// criar um Conta2.html
+//  instanciar controler e vai ter que chamar o saldo
