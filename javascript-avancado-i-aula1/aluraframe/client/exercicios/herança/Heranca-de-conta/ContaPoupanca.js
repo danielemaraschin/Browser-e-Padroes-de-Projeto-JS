6f9c678abfc442f4499a2027a2c2c195963b8e42
danielemaraschin <danikmaraschin@gmail.com>
2020-11-02 16:19:01 +0100

    working with inheritance

--- javascript-avancado-i-aula1/aluraframe/client/exercicios/herança/Heranca-de-conta/ContaPoupanca.js
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/herança/Heranca-de-conta/ContaPoupanca.js
@@ -3,7 +3,7 @@ class ContaPoupanca extends Conta{
     atualiza(taxa){
         this._saldo = this._saldo + (taxa*2);
     }
-    atualiza2(taxa){
+    /*atualiza2(taxa){
         return this._saldo + (taxa*2);
-    }
+    }*/
 }
