e467550f2bd1c1f0fc7362929c76dff8f9d47027
danielemaraschin <danikmaraschin@gmail.com>
2020-11-03 11:21:02 +0100

    Adding a new constructor property

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -3,13 +3,13 @@ class ListaNegociacoes {
     constructor(armadilha){
 
         this._negociacoes = []      //a lista inicia em branco
-        //this._armadilha = armadilha;
+        this._armadilha = armadilha;
         this._contexto = contexto;
     }
 
     adiciona(negociacao){               // pelo metodo adiciona ,add novas negociações
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
+        //this._armadilha(this);
     }
 
     get negociacoes(){                   
