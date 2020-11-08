89a91b603a6bd44bd959db8b4c6b9c968c2df1e9
danielemaraschin <danikmaraschin@gmail.com>
2020-11-08 22:14:35 +0100

    set function needs a iniciation variable value

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -2,12 +2,13 @@ class ListaNegociacoes {
 
     constructor(){
 
-        this._negociacoes = []      //a lista inicia em branco
+        this._negociacoes = [];     //a lista inicia em branco
         
     }
 
-    adiciona(negociacao){               // pelo metodo adiciona ,add novas negociações
-        this._negociacoes.push(negociacao);
+    adiciona(negociacao){                                             //this._negociacoes é a lista e dai concatena com a negociacao que esta add
+        this._negociacoes = [].concat(this._negociacoes, negociacao) // "força iniciar um valor para conseguir usar o set function"
+        //this._negociacoes.push(negociacao);// inicia sem valor inicial   ----era o método antigo, trocamos para conseguir lidar com a view
   
     }
 
