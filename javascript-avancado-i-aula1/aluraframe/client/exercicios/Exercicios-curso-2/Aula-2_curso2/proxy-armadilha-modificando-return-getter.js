d0fa1572ea73c00e397243f4c0868a6cc67bce45
danielemaraschin <danikmaraschin@gmail.com>
2020-11-11 12:25:49 +0100

    working with proxy, return getter...

---
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/Exercicios-curso-2/Aula-2_curso2/proxy-armadilha-modificando-return-getter.js
@@ -0,0 +1,13 @@
+let funcionario = {
+    email: 'abc@abc.com'
+};
+
+let proxyFuncionario = new Proxy(funcionario,{
+    get(target, prop, receiver){
+        console.log("Armadilha aqui");
+        return '**' + Reflect.get(target, prop, receiver) +'**' ;   
+    }                                                  
+    
+});
+
+console.log(proxyFuncionario.email);
