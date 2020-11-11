c6bd8bee668a868c6e7b086f0980aec9bab8f8ab
danielemaraschin <danikmaraschin@gmail.com>
2020-11-11 12:14:00 +0100

    working with proxy and Reflect.get

--- javascript-avancado-i-aula1/aluraframe/client/exercicios/Exercicios-curso-2/Aula-2_curso2/proxy-armadilha.js
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/Exercicios-curso-2/Aula-2_curso2/proxy-armadilha.js
@@ -6,7 +6,7 @@ let proxyFuncionario = new Proxy(funcionario,{
     get(target, prop, receiver){
         console.log("Armadilha aqui");
         return Reflect.get(target, prop, receiver);     // essa instrução que efetivamente realiza a operação no objeto real. 
-    }
+    }                                                   //get retorna uma função que é invocada para obter o valor da propriedade original
     
 });
 
