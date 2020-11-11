3eea2cba2906bb298fd7caada74df42305d72e4d
danielemaraschin <danikmaraschin@gmail.com>
2020-11-11 12:09:31 +0100

    working with proxy, reflect.get, console.log

--- javascript-avancado-i-aula1/aluraframe/client/exercicios/Exercicios-curso-2/Aula-2_curso2/proxy-armadilha.js
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/Exercicios-curso-2/Aula-2_curso2/proxy-armadilha.js
@@ -1,1 +1,13 @@
-let funcionario = {email: 'abc@abc.com'};
+let funcionario = {
+    email: 'abc@abc.com'
+};
+
+let proxyFuncionario = new Proxy(funcionario,{
+    get(target, prop, receiver){
+        console.log("Armadilha aqui");
+        return Reflect.get(target, prop, receiver);     // essa instrução que efetivamente realiza a operação no objeto real. 
+    }
+    
+});
+
+console.log(proxyFuncionario.email);
