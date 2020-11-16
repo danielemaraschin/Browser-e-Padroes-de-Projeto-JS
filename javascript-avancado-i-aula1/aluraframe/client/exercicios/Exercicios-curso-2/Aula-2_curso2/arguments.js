f621eb3886874433fb9c3bd022b3965f3e32b77d
danielemaraschin <danikmaraschin@gmail.com>
2020-11-16 15:15:10 +0100

    working with arguments

---
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/Exercicios-curso-2/Aula-2_curso2/arguments.js
@@ -0,0 +1,13 @@
+function exibeNomeCompleto() {
+
+    alert(`${arguments[0]} ${arguments[1]}`);
+  }
+  
+  exibeNomeCompleto('Flávio', 'Almeida');
+
+
+  /*
+  Podemos ter acesso aos parâmetros passados com 'arguments'
+  que é uma var implícita que nos dá acesso a todos
+  os parâmetros passados para a função ou método. 
+  */
