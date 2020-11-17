5e4bf28b526d04675e016e2bef9f05fb58b23a71
danielemaraschin <danikmaraschin@gmail.com>
2020-11-17 08:43:55 +0100

    working with Proxy Factory

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,44 +6,33 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        let self = this;
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-            get(target, prop, receiver) {                                                                               //verifica se dentro de prop, a propriedade que está sendo lida é uma das que quer interceptar (se está dentro do array),
-                if (['adiciona', 'esvazia'].includes(prop) && typeof (target[prop]) == typeof (Function)){               //verifica se a prop de target é uma função
-                    return function () {                                                                              // se tudo acima for verdadeiro substitui o método do proxy por outro (pela funcao abaixp reflect.apply)
-                        console.log(`interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);               //arguments é um parametro acessivel em qualquer funcao, que dá acesso a todos os parametros da função 
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return Reflect.get(target, prop, receiver);                                                              //se nao for verdadeiro o if, entao vem pra cá, dá reflect get e deixa o código seguir
-            }
-        });
+        this._listaNegociacoes = ProxyFactory.create(
+            new ListaNegociacoes(),
+            ['adiciona', '[esvazia]'], (model) =>  // nao precisa de parenteses nesse model pq eh so 1 parametro
+            this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
 
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#MensagemView'));                                                          //colocar dentro dos parenteses onde quer add a msg (no html )
+        this._mensagem = ProxyFactory.create(
+            new Mensagem(),['texto'], model =>    // nao precisa de parenteses nesse model pq eh so 1 parametro
+            this._mensagemView.update(model)
+            );
+        this._mensagemView = new MensagemView($('#MensagemView'));   //colocar dentro dos parenteses onde quer add a msg (no html )
         this._mensagemView.update(this._mensagem);
     }
     adiciona(event) {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-
         this._mensagem.texto = 'Negociação adicionada com sucesso.';
-
         this._limpaFormulario();
     }
 
     apaga() {
 
         this._listaNegociacoes.esvazia();
-
         this._mensagem.texto = 'Negociações apagadas com sucesso.';
-        this._mensagemView.update(this._mensagem);
-
     }
 
     _criaNegociacao() {
---
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/services/ProxyFactory.js
@@ -0,0 +1,19 @@
+class ProxyFactory {
+    static create (objeto, props, acao){
+
+        return new Proxy(new ListaNegociacoes(), {
+            get(target, prop, receiver) {                                                                               //verifica se dentro de prop, a propriedade que está sendo lida é uma das que quer interceptar (se está dentro do array),
+                if (props.includes(prop) && typeof (target[prop]) == typeof (Function)){               //verifica se a prop de target é uma função
+                    return function () {                                                                              // se tudo acima for verdadeiro substitui o método do proxy por outro (pela funcao abaixp reflect.apply)
+                        console.log(`interceptando ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);               //arguments é um parametro acessivel em qualquer funcao, que dá acesso a todos os parametros da função 
+                        return acao(target);
+                    }
+                }
+                return Reflect.get(target, prop, receiver);                                                              //se nao for verdadeiro o if, entao vem pra cá, dá reflect get e deixa o código seguir
+            }
+        });
+    }
+}
+
+//self._negociacoesView.update = return acao
