81ca16b323f95e852c15c5be26347ee03d0cec5d
danielemaraschin <danikmaraschin@gmail.com>
2020-11-17 18:10:41 +0100

    ProxyFactory to different objects

--- javascript-avancado-i-aula1/aluraframe/client/js/app/services/ProxyFactory.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/services/ProxyFactory.js
@@ -1,7 +1,7 @@
 class ProxyFactory {
     static create (objeto, props, acao){
 
-        return new Proxy(new ListaNegociacoes(), {
+        return new Proxy(objeto, {
             get(target, prop, receiver) {                                                                               //verifica se dentro de prop, a propriedade que está sendo lida é uma das que quer interceptar (se está dentro do array),
                 if (props.includes(prop) && typeof (target[prop]) == typeof (Function)){               //verifica se a prop de target é uma função
                     return function () {                                                                              // se tudo acima for verdadeiro substitui o método do proxy por outro (pela funcao abaixp reflect.apply)
