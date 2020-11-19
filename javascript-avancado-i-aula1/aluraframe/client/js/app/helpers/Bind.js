35eb2924ce554cb8d3b2c5c921b17b888cdaadb2
danielemaraschin <danikmaraschin@gmail.com>
2020-11-19 13:11:53 +0100

    Learning about bind and proxy

--- javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/Bind.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/Bind.js
@@ -1,10 +1,12 @@
 class Bind{
     constructor(model, view, props){
 
-        let proxy = ProxyFactory.create(model, props, AÇÃO){
-
-        }
+        let proxy = ProxyFactory.create(model, props, model =>  //cria um proxy com esse model,essa props, 
+            view.update(model));                    //quando elas forem acessadas faz a acao da arrow function: atualizar tela
         
+        view.update(model);    // atualiza a tela antes de devolver o proxy
+
+        return proxy;       // retorna o proxy;
     }
     
 }
