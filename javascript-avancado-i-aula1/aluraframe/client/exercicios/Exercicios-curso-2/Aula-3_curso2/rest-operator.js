a9a9ebe6c91e29059b6df5f33d5a7a356bffd3da
danielemaraschin <danikmaraschin@gmail.com>
2020-11-22 17:33:27 +0100

    Working with rest parameter

---
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/Exercicios-curso-2/Aula-3_curso2/rest-operator.js
@@ -0,0 +1,13 @@
+class Cesta{
+
+    constructor(tipo, ...items) {
+        //lógica - não é o que importa agora
+    
+    }
+}
+
+
+let cesta = new Cesta('fruta', 'banana', 'tomate', 'maça');
+
+var tipo = 'fruta';
+var itens =['banana', 'tomate', 'maçã'];
