2ddb3dc9cfd9e3573ab9108429193ce1f17bbd6e
danielemaraschin <danikmaraschin@gmail.com>
2020-11-28 19:51:46 +0100

    working with  Date

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -39,7 +39,7 @@ class NegociacaoController {
         if(xhr.readyState ==4) {
             if(xhr.status ==200) {
                 JSON.parse(xhr.responseText) // acesso às requisições retornadas do servidor.
-                .map(objeto => new Negociacao (objeto.data, objeto.quantidade, objeto.valor)) //Transforma JSON em objeto JS= pegar um array, varrer o array e criar um novo array MODIFICADO
+                .map(objeto => new Negociacao (New Date(objeto.data), objeto.quantidade, objeto.valor)) //Transforma JSON em objeto JS= pegar um array, varrer o array e criar um novo array MODIFICADO
                 .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao)); //Adicionar cada item desse novo array na lista de negociacoes
                 this._mensagem.texto = "Negociacões importadas com sucesso."
             }else{
