4ad91abf147c9536aa1508d2ff459ea7e39d9516
danielemaraschin <danikmaraschin@gmail.com>
2020-11-29 15:02:42 +0100

    working with services

--- javascript-avancado-i-aula1/aluraframe/client/js/app/services/NegociacaoService.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/services/NegociacaoService.js
@@ -10,10 +10,8 @@ class NegociacaoService {
                   JSON.parse(xhr.responseText) // acesso às requisições retornadas do servidor.
                   .map(objeto => new Negociacao (New Date(objeto.data), objeto.quantidade, objeto.valor)) //Transforma JSON em objeto JS= pegar um array, varrer o array e criar um novo array MODIFICADO
                   .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao)); //Adicionar cada item desse novo array na lista de negociacoes
-                  this._mensagem.texto = "Negociacões importadas com sucesso."
               }else{
                   console.log(xhr.responseText);
-                  this._mensagem.texto = 'Não foi possível obter as negociações.'
               }
           }
         };
