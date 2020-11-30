ad8b5c33fdf32865483457ce3196ab54aed5afbf
danielemaraschin <danikmaraschin@gmail.com>
2020-11-30 15:48:37 +0100

    Training about form, JSON, ...

--- javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/Bind.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/Bind.js
@@ -2,7 +2,7 @@ class Bind{
     constructor(model, view, ...props){
 
         let proxy = ProxyFactory.create(model, props, model =>  //cria um proxy com esse model,essa props, 
-            view.update(model));          //quando elas forem acessadas faz a acao da arrow function acima: atualizar tela (view.update(model))
+            view.update(model));          //quando elas forem acessadas faz a acao da arrow function: atualizar tela (view.update(model))
         
         view.update(model);    // atualiza a tela antes de devolver o proxy
 
