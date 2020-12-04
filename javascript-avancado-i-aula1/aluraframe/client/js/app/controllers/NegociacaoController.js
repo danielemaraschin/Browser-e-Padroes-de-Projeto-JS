2bb9c50f9445139d9f16cb14e8dde9df19e648f1
danielemaraschin <danikmaraschin@gmail.com>
2020-12-04 08:13:30 +0100

    Fixing the code

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,69 +1,64 @@
 class NegociacaoController {
-
+    
     constructor() { //QUANDO uma controler eh criada..
-
+        
         let $ = document.querySelector.bind(document);
+        
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
+         
         this._listaNegociacoes = new Bind(
-            new ListaNegociacoes(),
-            new NegociacoesView($('#negociacoesView')),
-            'adiciona', 'esvazia')    //METODOS
-
-
+            new ListaNegociacoes(), 
+            new NegociacoesView($('#negociacoesView')), 
+            'adiciona', 'esvazia');             //METODOS
+       
         this._mensagem = new Bind(
-            new Mensagem(),                        //modelo
-            new MensagemView($('#MensagemView')), //view   -colocar dentro dos parenteses onde quer add a msg (no html ) 
-            'texto');                             //condição  (PROPERTY)
+            new Mensagem(),                         //modelo
+            new MensagemView($('#mensagemView')),   //view   -colocar dentro dos parenteses onde quer add a msg (no html ) 
+            'texto');                               //condição  (PROPERTY)
     }
-
-
-    adiciona(event) {
-        event.preventDefault();
-
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso.';
-        this._limpaFormulario();
-    }
-
-    importaNegociacoes() {
-
-        let service = new NegociacaoService();
     
-        Promise.all([
-            service.obterNegociacoesDaSemana(),
-            service.obterNegociacoesDaSemanaAnterior(),
-            service.obterNegociacoesDaSemanaRetrasada()]
-        ).then(negociacoes => {
-            negociacoes
-              .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
-              .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações importadas com sucesso';
-        })
-        .catch(erro => this._mensagem.texto = erro);  
+    adiciona(event) {
+        
+        event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = 'Negociação adicionada com sucesso'; 
+        this._limpaFormulario();   
     }
-
-apaga() {
-
-    this._listaNegociacoes.esvazia();
-    this._mensagem.texto = 'Negociações apagadas com sucesso.';
-}
-
-_criaNegociacao() {
-    return new Negociacao(
-        DateHelper.textoParaData(this._inputData.value),
-        this._inputQuantidade.value,
-        this._inputValor.value
-    );
-}
-
-_limpaFormulario() {     // tem underline na frente pq somente a classe ListaNegociacao pode chamar esse método ( não faz sentido outra classe chamá-lo)
-
-    this._inputData.value = "";
-    this._inputQuantidade.value = 1;
-    this._inputValor.value = 0.0;
-    this._inputData.focus();
-}
+    
+    importaNegociacoes() {
+        
+        
+        let service = new NegociacaoService();
+        service
+            .obterNegociacoes()
+            .then(negociacoes => negociacoes.forEach(negociacao => {
+                this._listaNegociacoes.adiciona(negociacao);
+                this._mensagem.texto = 'Negociações do período importadas'   
+            }))
+            .catch(erro => this._mensagem.texto = erro);               
+    }
+    
+    apaga() {
+        
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+    }
+    
+    _criaNegociacao() {
+        
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);    
+    }
+    
+    _limpaFormulario() {  // tem underline na frente pq somente a classe ListaNegociacao pode chamar esse método ( não faz sentido outra classe chamá-lo)
+     
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();   
+    }
 }
