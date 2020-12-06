58254d647f55f752f5e68cb3c662e964d397438a
danielemaraschin <danikmaraschin@gmail.com>
2020-12-06 14:23:35 +0100

    Working with reduce,concat, map, arrow function...

--- javascript-avancado-i-aula1/aluraframe/client/exercicios/Exercicios-curso-2/aula-5_curso2/array-uma-dimensao.js
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/Exercicios-curso-2/aula-5_curso2/array-uma-dimensao.js
@@ -13,12 +13,11 @@ let dadosServidor = [
     ]        
 ];
 
-let listaDeNegociacoes = dadosServidor.reduce((novoArray, array) => {
-    // novoArray receberá os itens de array, no final terá uma dimensão apenas
-    return novoArray.concat(array)
-}, [])
-.map(dado => {
-    // para cada dado, cria uma instância de negociação. No final, teremos uma nova lista só com instâncias de Negociacao
+  // novoArray receberá os itens de array, no final terá uma dimensão apenas
+let listaDeNegociacoes = dadosServidor
+.reduce((novoArray, array)  => novoArray.concat(array), [])
+.map(dado => 
+    new Negociacao(new Date(dado.data), dado.quantidade, dado.valor ));               
 
-    return new Negociacao(new Date(dado.data), dado.quantidade, dado.valor )
-});
+
+// para cada dado( cada item/indice do novo array), cria uma instância de negociação. No final, teremos uma nova lista só com instâncias de Negociacao
