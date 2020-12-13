94d346c40bd25a43ac5db58263bdb94f51e92704
danielemaraschin <danikmaraschin@gmail.com>
2020-12-13 20:35:24 +0100

    working with array sort

--- javascript-avancado-i-aula1/aluraframe/client/exercicios/Exercicios-curso-2/Aula-6-curso-2/array-sort.reverse2.js
+++ javascript-avancado-i-aula1/aluraframe/client/exercicios/Exercicios-curso-2/Aula-6-curso-2/array-sort.reverse2.js
@@ -1,11 +1,3 @@
-/*let lista = [10,1, 5, 9, 8, 12, 15];
-
-function comparaNumeros(a,b) { if (a == b) return 0; if (a < b) return -1; if (a > b) return 1; }
-var ordem = lista.sort(comparaNumeros);
-var inversa = ordem.reverse
-console.log(inversa); // exibe a lista na ordem decrescente
-*/
-
 let lista = [10,1, 5, 9, 8, 12, 15];
 
 function comparaNumeros(a,b) { if (a == b) return 0; if (a < b) return -1; if (a > b) return 1; }
